70691c528f47478237974676e48600080542ca27
Wamique Abro <wamik.abro212@gmail.com>
2024-06-10 20:18:14 +0500

    arrays

---
+++ Array/script.js
@@ -0,0 +1,12 @@
+// it is sorting alphabatically even if we use numbers
+console.log(["c", "a", "d", "b"].sort());
+
+// it won't sort like 2 50 100, because it's looking at them as if they're strings
+// so it will think 100 is smallest since it's first letter is 1
+console.log([100, 2, 50].sort());
+
+// it's gonna return string
+console.log(["lions", "tigers", "bears oh my!"].join(" & "));
+
+// it takes 2 arrays, and return single array concatinating
+console.log([1, 2, 3].concat([4, 5, 6]));
