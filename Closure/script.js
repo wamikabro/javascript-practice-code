5a5fb0762ecc42fc738dc870df14c95630ef1696
Wamique Abro <wamik.abro212@gmail.com>
2024-07-07 20:44:16 +0500

    recall outer function to create new closure

--- Closure/script.js
+++ Closure/script.js
@@ -10,3 +10,9 @@ const counter = createCounter();
 console.log(counter()); // 1
 console.log(counter()); // 2
 console.log(counter()); // 3
+
+// It will have different backpack.
+const counter2 = createCounter();
+console.log(counter2()); // 1
+console.log(counter2()); // 2
+console.log(counter2()); // 3
