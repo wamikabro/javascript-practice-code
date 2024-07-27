b3a913424623008f1b13ff28b49ce9cb393c9ff7
Wamique Abro <wamik.abro212@gmail.com>
2024-07-27 20:25:18 +0500

    minus minus vs minus on string

---
+++ Deep/minus minus vs minus/script.js
@@ -0,0 +1,6 @@
+let string = "5";
+
+string = string - 1; // 4 because - operator is converting it into a number
+console.log(string);
+string--; // 3, because -- is converting it into a number first
+console.log(string);
