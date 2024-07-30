52d31a0a6586db78b5848a93d73f95f2423295e6
Wamique Abro <wamik.abro212@gmail.com>
2024-07-30 17:32:40 +0500

    automatic coercion

---
+++ Deep/automatic coercion/script.js
@@ -0,0 +1,11 @@
+console.log(1 + 1); // 2
+console.log(typeof (1 + 1));
+
+console.log(1 + "1"); // 11
+console.log(typeof (1 + "1"));
+
+console.log(1 + 1 + ""); // 2
+console.log(typeof (1 + 1 + ""));
+
+console.log(1 + (1 + "")); // 11
+console.log(typeof (1 + (1 + "")));
