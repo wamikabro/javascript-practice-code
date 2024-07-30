10e1d10ea62ef54acbf3ac00ee4a85b83b2ec9b6
Wamique Abro <wamik.abro212@gmail.com>
2024-07-30 17:34:05 +0500

    add comments to the code

--- Deep/automatic coercion/script.js
+++ Deep/automatic coercion/script.js
@@ -1,11 +1,11 @@
 console.log(1 + 1); // 2
-console.log(typeof (1 + 1));
+console.log(typeof (1 + 1)); // number
 
 console.log(1 + "1"); // 11
-console.log(typeof (1 + "1"));
+console.log(typeof (1 + "1")); // string
 
 console.log(1 + 1 + ""); // 2
-console.log(typeof (1 + 1 + ""));
+console.log(typeof (1 + 1 + "")); // string
 
-console.log(1 + (1 + "")); // 11
-console.log(typeof (1 + (1 + "")));
+console.log(1 + (1 + "")); // 11 : brackets changed the result
+console.log(typeof (1 + (1 + ""))); // string
