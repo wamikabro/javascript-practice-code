73ba73e8777f505c2860b67cbec4b90d00eccf9d
Wamique Abro <wamik.abro212@gmail.com>
2024-09-24 11:34:31 +0500

    try Object.create

--- Objects/script.js
+++ Objects/script.js
@@ -9,5 +9,8 @@ person.last_name = "Abro";
 
 console.log(person);
 
-// freeze an object so some options are frozen/not available anymore
-person.freeze();
+// Use object.create to set __proto__
+const newObject = Object.create(person); // setting person object as prototype of newObject
+console.log(newObject); // it will be chained to the person
+// proof
+console.log(newObject.first_name); // calling the property of person
